c8f622b053b9d8a58ad020480997f5ffd760c0dc
devryan <ripanbaidya7980@gmail.com>
2025-06-25 23:59:41 +0530

    variables in js

---
+++ js/basics/variables.js
@@ -0,0 +1,27 @@
+/**
+ * Variables:
+ * Variables in javascript are containers for storing data values.
+ */
+
+let x = 10;
+let y = 20;
+
+// assigning values to variables
+let name = "Ripan Baidya";
+let age = 20;
+let isEmployed = true;
+let salary = 1000000.00;
+let address = null;
+
+// checking the type of variables
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isEmployed);
+console.log(typeof salary);
+console.log(typeof address);
+
+// print the value of variables
+console.log(`Name is ${name}`);
+console.log(`Age is ${age}`);
+console.log(`Is employes? ${isEmployed}`);
+console.log(`Salary is ${salary}`);
