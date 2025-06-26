156877dae5bf3bcc05f6e04310b007851cc06f1f
devryan <ripanbaidya7980@gmail.com>
2025-06-26 10:53:55 +0530

    add operators example in js

---
+++ js/basics/arithmetic-operator.js
@@ -0,0 +1,14 @@
+/**
+ * In this section we will learn about arithmetic operators in JavaScript.
+ * Arithmetic operators are used to perform mathematical operations on numbers.
+ */
+
+let a = 10;
+let b = 4;
+
+console.log("Addition:", a + b);        // Addition: 15
+console.log("Subtraction:", a - b);     // Subtraction: 5
+console.log("Multiplication:", a * b);  // Multiplication: 50
+console.log("Division:", a / b);        // Division: 2
+console.log("Modulus:", a % b);         // Modulus: 0
+console.log("Exponentiation:", a ** b); // Exponentiation: 100000
---
+++ js/basics/logical-operator.js
@@ -0,0 +1,23 @@
+/**
+ * In this section, we will explore the logical operators in JavaScript.
+ * Logical operators are used to combine two or more expressions and return a boolean value (true or false).
+ * The three main logical operators in JavaScript are:
+ * - AND (&&)
+ * - OR (||)
+ * - NOT (!)
+ */
+
+let a = true;
+let b = false;
+
+
+// AND operator (&&)
+console.log(a && b); // false
+console.log(a && true); // true
+
+// OR operator (||)
+console.log(a || b); // true
+console.log(b || false); // false
+
+// NOT operator (!)
+console.log(!a); // false
